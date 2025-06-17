fdb550d6649abf07dcf1c2c563aa212ccc96a47a
Gabriel Julio <gabrieldasilvajulio@gmail.com>
2025-06-17 18:37:55 -0300

    :sparkles: Adicionando Componenente Home estilizado

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,37 +1,23 @@
+import './Home.css';
+
 function Home() {
     return (
-        <>
-            <div style={{
-                width: "100vw",
-                display: "flex",
-                justifyContent: "center"
-            }}>
-                <div>
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
-                        <h2>Seja Bem Vinde!</h2>
-                        <p>Expresse aqui seus pensamentos e opniões</p>
-                    </div>
+        <div className='container'>
+            <div>
+                <div className='texto_superior'>
+                    <h2>Seja Bem Vinde!</h2>
+                    <p>Expresse aqui seus pensamentos e opniões</p>
+                </div>
 
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
-                        <img 
-                            src="https://i.imgur.com/VpwApCU.png" 
-                            alt="Imagem da Página Home" 
-                            width="400px"
-                        />
-                    </div>
+                <div>
+                    <img 
+                        src="https://i.imgur.com/VpwApCU.png" 
+                        alt="Imagem da Página Home" 
+                        width="400px"
+                    />
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
